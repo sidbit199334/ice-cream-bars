484736896bdc90a44fbb31f4174de84ed4ffd8ba
Sidhartha Bramandlapalli <sidbittu199334@gmail.com>
2022-09-29 18:50:00 -0400

    added day1 Code

---
+++ src/components/Designer.tsx
@@ -0,0 +1,17 @@
+import "./Designer.css";
+const Designer = () => {
+  return (
+    <header className="Designer">
+      <p>Ad Designer</p>
+      <p>Vote for Strawberry</p>
+      <p>What to support</p>
+      <p>chocolate vanilla strawberry</p>
+      <p>Color Theme</p>
+      <p>light dark</p>
+      <p>Font Size</p>
+      <p>DOwn 44 Up</p>
+    </header>
+  );
+};
+
+export default Designer;
---
+++ src/components/Vote.tsx
@@ -0,0 +1,14 @@
+import "./Vote.css";
+const Vote = () => {
+  return (
+    <header className="Vote">
+      <h1>Vote Here</h1>
+      <p>chocolate vanilla strawberry</p>
+      <p>chocolate</p>
+      <p>strawberry</p>
+      <p>vanilla</p>
+    </header>
+  );
+};
+
+export default Vote;
